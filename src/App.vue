acda6f51d27712767d9e05a36d3c449e2a5fe7b1
Maxime Desmarais <maxime.desmarais1610@gmail.com>
2019-10-02 21:25:22 +0200

    Ajout d'une carte affichant le tracé de la course

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <map-component />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import MapComponent from './components/MapComponent.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            MapComponent
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    html, body, #app {
+        height: 100%;
+        width: 100%;
+    }
 </style>
---
+++ src/components/MapComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="raceMap">
+        <l-map :zoom="zoom" :center="center">
+            <l-tile-layer :url="tileUrl" :attribution="attribution" />
+            <l-polyline :lat-lngs="segment" v-for="segment in segments" />
+            <l-polyline :lat-lngs="transition" :color="transitionsColor" v-for="transition in transitions" />
+        </l-map>
+    </div>
+</template>
+<script>
+    import { LMap, LTileLayer, LPolyline } from 'vue2-leaflet'
+    import { S1, S2, S3, S4, S5, TS12, TS23, TS34, TS45 } from '../segments.js'
+
+    export default {
+        components: {
+            LMap,
+            LTileLayer,
+            LPolyline
+        },
+        data () {
+            return {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+                center: [48.1266, -1.6694],
+                tileUrl: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+                zoom: 13,
+
+                segments: [ S1, S2, S3, S4, S5 ],
+                transitions: [ TS12, TS23, TS34, TS45 ],
+                transitionsColor: "black"
+            }
+        }
+    }
+</script>
+
+<style>
+    #raceMap {
+        height: 100%;
+    }
+</style>
